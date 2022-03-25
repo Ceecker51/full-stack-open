e74e79ffeff335d8177514ecb214a2a8e574eb68
Marco Goebel <marco@gmpteam.de>
2022-03-25 15:14:57 +0100

    Add exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,10 +8,10 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   if (text === "positive") {
-    return <div>{text} {value} %</div>
+    return <tr><td>{text}</td><td>{value} %</td></tr>
   }
 
-  return <div>{text} {value}</div>
+  return <tr><td>{text}</td><td>{value}</td></tr>
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -26,14 +26,16 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive} />
+      </tbody>
+    </table>
   )
 }
 
