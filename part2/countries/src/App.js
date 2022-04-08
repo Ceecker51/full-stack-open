8bc3440e025662b3946f074904d35305496db2d6
goebelma86867 <goebelma86867@th-nuernberg.de>
2022-04-08 13:42:08 +0200

    Add exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,31 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Weather = ({ capital }) => {
+  const [weather, setWeather] = useState()
+
+  useEffect(() => {
+    axios
+      .get(`http://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+      .then(response => {
+        setWeather(response.data);
+      })
+  }, []);
+
+  if (!weather) {
+    return <p>Loading ...</p>
+  }
+
+  return (
+    <div>
+      <h2>Weather in {capital}</h2>
+      <div>temperature {(weather.main.temp - 273.15 /*K*/).toFixed(2)} Celcius</div>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+      <div>wind {weather.wind.speed} m/s</div>
+    </div>
+  )
+}
+
 const CountryDetails = ({ country }) => {
   const languages = Object.values(country.languages);
 
@@ -14,6 +39,7 @@ const CountryDetails = ({ country }) => {
         {languages.map((language, i) => <li key={i}>{language}</li>)}
       </ul>
       <img width="150" height="150" src={country.flags.svg} alt={country.flag} />
+      <Weather capital={country.capital} />
     </div>
   )
 }
