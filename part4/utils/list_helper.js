c2a0a6ec014e80cc6f37075be1b5fe9183a7ffbb
Marco Goebel <marco@gmpteam.de>
2022-08-21 19:48:38 +0200

    Add exercise 4.6 without lodash

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -14,8 +14,30 @@ const favoriteBlog = (blogs) => {
   return { title, author, likes };
 };
 
+const mostBlogs = (blogs) => {
+  const authorBlogs = {};
+
+  for (const blog of blogs) {
+    if (!authorBlogs[blog.author]) {
+      authorBlogs[blog.author] = 0;
+    }
+
+    authorBlogs[blog.author] = authorBlogs[blog.author] + 1;
+  }
+
+  const maxEntry = Object.entries(authorBlogs).reduce((prev, curr) =>
+    prev.blogs > curr.blogs ? prev : curr
+  );
+
+  return {
+    author: maxEntry[0],
+    blogs: maxEntry[1],
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
