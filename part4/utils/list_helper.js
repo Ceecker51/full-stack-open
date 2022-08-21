8830aef5da444efaaaa6c7ad003e7ebfe7bf3850
Marco Goebel <marco@gmpteam.de>
2022-08-21 19:16:17 +0200

    Add exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => acc + curr.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const { title, author, likes } = blogs.reduce((prev, curr) =>
+    prev.likes > curr.likes ? prev : curr
+  );
+
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
