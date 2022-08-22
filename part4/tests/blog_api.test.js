aaf60ca7b9027f127697068a7bd34e20e313873b
Marco Goebel <marco@gmpteam.de>
2022-08-22 15:45:06 +0200

    Add exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -45,6 +45,32 @@ test('all notes are returned as json', async () => {
   expect(titles).toContain('TDD harms architecture');
 }, 100000);
 
+// #############################
+// POST /api/blogs
+// #############################
+
+test('a blog post can be added', async () => {
+  const newBlog = {
+    title: 'Learning JavaScript Design Patterns',
+    author: 'Addy Osmani',
+    url: 'https://www.patterns.dev/posts/classic-design-patterns/',
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogs = await Blog.find({});
+
+  const blogsAtEnd = blogs.map((blog) => blog.toJSON());
+  expect(blogsAtEnd).toHaveLength(testHelper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((blog) => blog.title);
+  expect(titles).toContain('Learning JavaScript Design Patterns');
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
