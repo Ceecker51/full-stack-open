3c9d64979f490b151e6e277007f4f3d425ad655a
Marco Goebel <marco@gmpteam.de>
2022-08-22 15:10:29 +0200

    Add exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+
+const testHelper = require('./test_helper');
+const app = require('../app');
+const api = supertest(app); // Wrap application into a superagent object
+
+const Blog = require('../models/blog');
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
+
+// #############################
+// GET /api/blogs
+// #############################
+
+test('all notes are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const titles = response.body.map((r) => r.title);
+
+  expect(titles).toHaveLength(testHelper.initialBlogs.length);
+  expect(titles).toContain('TDD harms architecture');
+}, 100000);
+
+afterAll(() => {
+  mongoose.connection.close();
+});
