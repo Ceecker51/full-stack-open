ad498ea07346381085ff6aa6036e01ac135e8913
Marco Goebel <marco@gmpteam.de>
2022-08-23 15:14:08 +0200

    Add exercise 4.15

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+
 const logger = require('./utils/logger');
 
 const mongoose = require('mongoose');
@@ -13,6 +15,7 @@ const mongoose = require('mongoose');
 // ########################
 
 logger.info('connecting to', config.MONGODB_URI);
+
 mongoose.connect(config.MONGODB_URI);
 
 // ########################
@@ -23,6 +26,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
---
+++ part4/controllers/users.js
@@ -0,0 +1,35 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body;
+
+  // validate username
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({ error: 'username must be unique' });
+  }
+
+  // calculate hash of password
+  const saltRounds = 10;
+  const pwdHash = await bcrypt.hash(password, saltRounds);
+
+  // create new user
+  const user = new User({
+    username,
+    name,
+    pwdHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
