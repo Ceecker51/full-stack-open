aa18162f425ebc63405a04bbaf4fca761230790e
Marco Goebel <marco@gmpteam.de>
2022-09-07 15:17:43 +0200

    Add exercise 5.11

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const BlogForm = ({ createBlog }) => {
   // ###############################
@@ -63,6 +64,10 @@ const BlogForm = ({ createBlog }) => {
       <button type="submit">create</button>
     </form>
   );
+};
+
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired,
 };
 
 export default BlogForm;
