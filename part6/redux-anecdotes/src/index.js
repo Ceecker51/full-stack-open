ca123c1826cae7f1f39f82e7304edf22db499515
Marco Goebel <marco@gmpteam.de>
2022-09-10 13:45:20 +0200

    Add exercise 6.9

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/anecdoteReducer';
+import store from './store';
 
 import App from './App';
-
-const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
