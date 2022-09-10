507f9cf161a074bbb3041ef9556c4220bab8e376
Marco Goebel <marco@gmpteam.de>
2022-09-10 17:06:49 +0200

    Add exercise 6.15

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -32,19 +32,9 @@ const AnecdoteList = () => {
     setTimeout(() => dispatch(removeNotification()), 5000);
   };
 
-  const sortByVotes = (a, b) => {
-    if (a.votes > b.votes) {
-      return -1;
-    } else if (a.votes < b.votes) {
-      return 1;
-    }
-
-    return 0;
-  };
-
   return (
     <div>
-      {anecdotes.sort(sortByVotes).map((anecdote) => (
+      {anecdotes.map((anecdote) => (
         <Anecdote
           key={anecdote.id}
           anecdote={anecdote}
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,47 +1,50 @@
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case 'VOTE':
-      const id = action.data.id;
+import { createSlice } from '@reduxjs/toolkit';
+
+import anecdoteService from '../services/anecdotes';
+
+const sortByVotes = (a, b) => {
+  if (a.votes > b.votes) {
+    return -1;
+  } else if (a.votes < b.votes) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: [],
+  reducers: {
+    createAnecdote(state, action) {
+      state.push(action.payload);
+    },
+    voteAnecdote(state, action) {
+      const id = action.payload;
       const anecdoteToChange = state.find((anecdote) => anecdote.id === id);
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
       };
 
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      );
+      return state
+        .map((anecdote) => (anecdote.id !== id ? anecdote : changedAnecdote))
+        .sort(sortByVotes);
+    },
+    setAnecdotes(state, action) {
+      return action.payload.sort(sortByVotes);
+    },
+  },
+});
 
-    case 'NEW_ANECDOTE':
-      return [...state, action.data];
+export const { createAnecdote, voteAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
 
-    case 'SET_ANECDOTES':
-      return action.data;
-
-    default:
-      return state;
-  }
-};
-
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    data: { id },
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
   };
 };
 
-export const createAnecdote = (anecdote) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: anecdote,
-  };
-};
-
-export const setAnecdotes = (anecdotes) => {
-  return {
-    type: 'SET_ANECDOTES',
-    data: anecdotes,
-  };
-};
-
-export default reducer;
+export default anecdoteSlice.reducer;
