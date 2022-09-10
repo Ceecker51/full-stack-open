42a4cb4900d7ecd0c53211071ab5ff5e68beadaf
Marco Goebel <marco@gmpteam.de>
2022-09-10 12:30:50 +0200

    Add exercise 6.7

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer';
+import { voteAnecdote } from './reducers/anecdoteReducer';
+
+import AnecdoteForm from './components/AnecdoteForm';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -7,13 +9,6 @@ const App = () => {
 
   const vote = (id) => {
     dispatch(voteAnecdote(id));
-  };
-
-  const addAnecdote = (event) => {
-    event.preventDefault();
-    const content = event.target.anecdote.value;
-    event.target.anecdote.value = '';
-    dispatch(createAnecdote(content));
   };
 
   const sortByVotes = (a, b) => {
@@ -39,12 +34,7 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
