bc54c05440c6ed3cb24fcc0d42b277d9d3a8e1f5
Marco Goebel <marco@gmpteam.de>
2022-09-10 12:25:44 +0200

    Add exercise 6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -17,10 +17,20 @@ const App = () => {
     dispatch(createAnecdote(content));
   };
 
+  const sortByVotes = (a, b) => {
+    if (a.votes > b.votes) {
+      return -1;
+    } else if (a.votes < b.votes) {
+      return 1;
+    }
+
+    return 0;
+  };
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {anecdotes.sort(sortByVotes).map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
