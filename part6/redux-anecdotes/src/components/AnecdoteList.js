13749534d487a9c0866200c35ed73ed688ddac80
Marco Goebel <marco@gmpteam.de>
2022-09-10 12:40:39 +0200

    Add exercise 6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  );
+};
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id));
+  };
+
+  const sortByVotes = (a, b) => {
+    if (a.votes > b.votes) {
+      return -1;
+    } else if (a.votes < b.votes) {
+      return 1;
+    }
+
+    return 0;
+  };
+
+  return (
+    <div>
+      {anecdotes.sort(sortByVotes).map((anecdote) => (
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => vote(anecdote.id)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
