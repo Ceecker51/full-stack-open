c705f5b26d635c3d6fba6986a54bf6c47c025ddf
Marco Goebel <marco@gmpteam.de>
2022-09-20 07:56:43 +0200

    Add exercise 6.16

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -5,8 +5,6 @@ import {
   removeNotification,
 } from '../reducers/notificationReducer';
 
-import anecdoteService from '../services/anecdotes';
-
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
@@ -15,8 +13,7 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value;
     event.target.anecdote.value = '';
 
-    const newAnecdote = await anecdoteService.create(content);
-    dispatch(createAnecdote(newAnecdote));
+    dispatch(createAnecdote(content));
     dispatch(setNotification(`you added ${content}`));
     setTimeout(() => dispatch(removeNotification()), 5000);
   };
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -31,19 +31,29 @@ const anecdoteSlice = createSlice({
         .map((anecdote) => (anecdote.id !== id ? anecdote : changedAnecdote))
         .sort(sortByVotes);
     },
+    appendAnecdote(state, action) {
+      state.push(action.payload);
+    },
     setAnecdotes(state, action) {
       return action.payload.sort(sortByVotes);
     },
   },
 });
 
-export const { createAnecdote, voteAnecdote, setAnecdotes } =
+export const { voteAnecdote, appendAnecdote, setAnecdotes } =
   anecdoteSlice.actions;
 
 export const initializeAnecdotes = () => {
   return async (dispatch) => {
     const anecdotes = await anecdoteService.getAll();
     dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.create(content);
+    dispatch(appendAnecdote(newAnecdote));
   };
 };
 
