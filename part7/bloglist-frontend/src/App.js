e4c89f985a9db7423dcfa6b05cbc05aafad39b9d
Marco Goebel <marco@gmpteam.de>
2022-09-23 08:46:23 +0200

    Add exercise 7.14

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,14 +1,17 @@
 import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 
 import Blog from './components/Blog';
 import LoginForm from './components/LoginForm';
 import BlogForm from './components/BlogForm';
 import Notification from './components/Notification';
 import Togglable from './components/Togglable';
+import UserList from './components/UserList';
 
 import { initializeBlogs, createBlog, deleteBlog, likeBlog } from './reducers/blogReducer';
 import { initializeUser, logout } from './reducers/authReducer';
+import { initializeUsers } from './reducers/userReducer';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -24,6 +27,7 @@ const App = () => {
   useEffect(() => {
     dispatch(initializeUser());
     dispatch(initializeBlogs());
+    dispatch(initializeUsers());
   }, [dispatch]);
 
   // ########################
@@ -70,23 +74,40 @@ const App = () => {
       <h2>blogs</h2>
       <Notification />
       <div>
-        <p>
-          {user.name} logged in <button onClick={handleLogout}>logout</button>
-        </p>
+        <p>{user.name} logged in</p>
+        <button onClick={handleLogout}>logout</button>
       </div>
-      <Togglable btnLabel="create new blog" ref={blogFormRef}>
-        <BlogForm createBlog={addBlog} />
-      </Togglable>
-
-      {blogs.map((blog) => (
-        <Blog
-          key={blog.id}
-          user={user}
-          blog={blog}
-          addLike={() => handleLike(blog.id)}
-          removeBlog={() => removeBlog(blog.id)}
+      <Routes>
+        <Route
+          path="/users"
+          element={
+            <div>
+              <h2>Users</h2>
+              <UserList />
+            </div>
+          }
         />
-      ))}
+        <Route
+          path="/"
+          element={
+            <div>
+              <Togglable btnLabel="create new blog" ref={blogFormRef}>
+                <BlogForm createBlog={addBlog} />
+              </Togglable>
+
+              {blogs.map((blog) => (
+                <Blog
+                  key={blog.id}
+                  user={user}
+                  blog={blog}
+                  addLike={() => handleLike(blog.id)}
+                  removeBlog={() => removeBlog(blog.id)}
+                />
+              ))}
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 };
--- part7/bloglist-frontend/src/store.js
+++ part7/bloglist-frontend/src/store.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import notificationReducer from './reducers/notificationReducer';
 import blogReducer from './reducers/blogReducer';
 import authReducer from './reducers/authReducer';
+import userReducer from './reducers/userReducer';
 
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
+    users: userReducer,
     user: authReducer,
     notification: notificationReducer,
   },
