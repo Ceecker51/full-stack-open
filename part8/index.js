d85ccec24af4c9e82fe597c5caef84e7962e5f05
Marco Goebel <marco@gmpteam.de>
2022-09-24 13:14:45 +0200

    Add exercise 8.7

--- part8/index.js
+++ part8/index.js
@@ -109,6 +109,7 @@ const typeDefs = gql`
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -139,7 +140,7 @@ const resolvers = {
   Mutation: {
     addBook: (root, args) => {
       const author = authors.find((a) => a.name === args.author);
-      
+
       if (!author) {
         const author = { id: uuid(), name: args.author };
         authors = authors.concat(author);
@@ -148,6 +149,17 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
