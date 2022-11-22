1f3bb92acae68604296f33169bb0462983209cbf
Marco Goebel <marco@gmpteam.de>
2022-11-22 10:16:29 +0100

    Add exercise 9.19

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,7 +1,50 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
-export interface Diagnose {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface DateRange {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: DateRange;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
@@ -24,3 +67,8 @@ export interface Patient {
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
+
+// Define special omit for unions
+//type UnionOmit<T, K extends string | number | symbol> = T extends unknown? Omit<T, K>: never;
+// Define Entry without the 'id' property
+//type EntryWithoutId = UnionOmit<Entry, 'id'>;
