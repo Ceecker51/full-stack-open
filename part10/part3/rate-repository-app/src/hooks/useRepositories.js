f57adecf21d7b6e0e08c2976925cce71d04529e7
Marco Goebel <marco@gmpteam.de>
2023-07-21 13:59:53 +0200

    Add exercise 10.11

--- part10/part3/rate-repository-app/src/hooks/useRepositories.js
+++ part10/part3/rate-repository-app/src/hooks/useRepositories.js
@@ -1,28 +1,13 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
 
-  const fetchRepositories = async () => {
-    const baseUrl = "http://192.168.3.26:5000";
-
-    setLoading(true);
-
-    const response = await fetch(`${baseUrl}/api/repositories`);
-    const json = await response.json();
-
-    //console.log(json);
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories: data ? data.repositories : undefined, loading };
 };
 
 export default useRepositories;
